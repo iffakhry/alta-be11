008bb8addac30fb83e4751ad0a772d8ee0732cc4
Fakhry-alta <fakhry.ikhsan.firdaus@gmail.com>
2022-08-04 11:59:56 +0700

    update branching.go

--- 1-var-data-type-branching/5-branching.go
+++ 1-var-data-type-branching/5-branching.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var suhu = 101
-	var masker = true
+	// var suhu = 101
+	// var masker = true
 	// if suhu == 36 {
 	// 	fmt.Println("suhu normal", suhu)
 	// }
@@ -18,20 +18,38 @@ func main() {
 	// 	fmt.Println("Anda demam")
 	// }
 
-	if suhu <= 36 && masker == true {
-		fmt.Println("normal")
-	} else if suhu > 37 && suhu < 40 {
-		fmt.Println("panas")
-	} else {
-		fmt.Println("waspada")
-	}
+	// if suhu <= 36 && masker == true {
+	// 	fmt.Println("normal")
+	// } else if suhu > 37 && suhu < 40 {
+	// 	fmt.Println("panas")
+	// } else {
+	// 	fmt.Println("waspada")
+	// }
 
-	if suhu == 35 {
-		fmt.Println("suhu 35")
-	} else if suhu > 100 {
-		fmt.Println("else if")
+	// if suhu == 35 {
+	// 	fmt.Println("suhu 35")
+	// } else if suhu > 100 {
+	// 	fmt.Println("else if")
+	// } else {
+	// 	fmt.Println("qqqqqq")
+	// }
+
+	nilai := 80
+	waktu := 4
+	if nilai > 85 {
+		fmt.Println("nilai A")
+		if waktu <= 4 {
+			fmt.Println("cumlaude")
+		} else {
+			fmt.Println("normal")
+		}
+	} else if nilai >= 70 && nilai <= 85 {
+		fmt.Println("B")
+		if waktu <= 4 {
+			fmt.Println("memuaskan")
+		}
 	} else {
-		fmt.Println("qqqqqq")
+		fmt.Println("C")
 	}
 
 }
