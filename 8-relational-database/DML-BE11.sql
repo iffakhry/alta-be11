16c75e3b50fa987586f242bb7e5711469c854700
Fakhry-alta <fakhry.ikhsan.firdaus@gmail.com>
2022-08-18 12:01:54 +0700

    add join resources

--- 8-relational-database/DML-BE11.sql
+++ 8-relational-database/DML-BE11.sql
@@ -16,7 +16,11 @@ VALUES ("Menggambar", 75);
 INSERT INTO data_mengajar (id, guru_id, mata_pelajaran_id, kelas, jam_mulai, jam_selesai)
 VALUES ("DM-0001",4, 1, "10A", "07:00", "09:00");
 INSERT INTO data_mengajar (id, guru_id, mata_pelajaran_id, kelas, jam_mulai, jam_selesai)
-VALUES ("DM-0004",6, 3, "12A", "09:00", "12:00");
+VALUES ("DM-0002",7, 3, "12A", "09:00", "12:00");
+INSERT INTO data_mengajar (id, guru_id, mata_pelajaran_id, kelas, jam_mulai, jam_selesai)
+VALUES ("DM-0003",7, 4, "12B", "07:00", "09:00");
+INSERT INTO data_mengajar (id, guru_id, mata_pelajaran_id, kelas, jam_mulai, jam_selesai)
+VALUES ("DM-0004",8, 5, "11A", "07:00", "09:00");
 
 -- membaca data
 SELECT * FROM guru;
@@ -70,6 +74,34 @@ SELECT g.id, g.nama, g.nip from guru g;
 SELECT guru.id, guru.nama, guru.nip from guru;
 SELECT g.id, g.nama AS nama_guru, g.nip from guru g;
 
--- join 
-SELECT g.id, g.nama, g.nip, dm.kelas from guru g
+-- inner join 
+-- menampilkan list jadwal seluruh guru yang sudah punya jadwal mengajar
+SELECT g.id, g.nama, g.nip, dm.kelas, dm.jam_mulai, dm.jam_selesai from guru g
 INNER JOIN data_mengajar dm ON g.id = dm.guru_id;
+
+SELECT g.id, g.nama, g.nip, dm.kelas, dm.jam_mulai, dm.jam_selesai, mp.nama_pelajaran from guru g
+INNER JOIN data_mengajar dm ON g.id = dm.guru_id
+INNER JOIN mata_pelajaran mp ON dm.mata_pelajaran_id = mp.id
+where dm.kelas like "%A"
+;
+
+SELECT g.id, g.nama, g.nip, mp.nama_pelajaran from guru g
+INNER JOIN data_mengajar dm ON g.id = dm.guru_id
+INNER JOIN mata_pelajaran mp ON dm.mata_pelajaran_id = mp.id
+;
+
+-- menampilkan list seluruh guru, baik yang punya jadwal maupun yang belum
+-- left join
+SELECT g.id, g.nama, dm.kelas, dm.jam_mulai from guru g
+LEFT JOIN data_mengajar dm ON g.id = dm.guru_id;
+
+SELECT g.id, g.nama, g.nip, dm.kelas, dm.jam_mulai, dm.jam_selesai from data_mengajar dm
+LEFT JOIN guru g ON g.id = dm.guru_id;
+
+-- right join
+SELECT g.id, g.nama, g.nip, dm.kelas, dm.jam_mulai, dm.jam_selesai from guru g
+RIGHT JOIN data_mengajar dm ON g.id = dm.guru_id
+WHERE dm.jam_mulai = "07:00";
+
+SELECT g.id, g.nama, g.nip, dm.kelas, dm.jam_mulai, dm.jam_selesai from data_mengajar dm
+RIGHT JOIN guru g ON g.id = dm.guru_id;
