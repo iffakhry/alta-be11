6fc2c9b2fa8e97c2243ef9b925edfebdfa8ade50
Fakhry-alta <fakhry.ikhsan.firdaus@gmail.com>
2022-08-20 11:14:38 +0700

    update config mysql-driver.go to check connection db

--- 9-raw-sql-go/structured-project/config/mysql-driver.go
+++ 9-raw-sql-go/structured-project/config/mysql-driver.go
@@ -11,10 +11,16 @@ import (
 func ConnectToDB() *sql.DB {
 	// membuat koneksi dari app golang ke DB
 	// <username>:<password>@tcp(<hostname>:<portDB>)/<db_name>
-	db, err := sql.Open("mysql", "root:qwerty123@tcp(127.0.0.1:3306)/db_be11")
+	db, err := sql.Open("mysql", "root:qwerty13@tcp(localhost:3306)/db_be11")
 
 	if err != nil {
-		log.Fatal("error", err.Error())
+		log.Fatal("error sql Open ", err.Error())
+	}
+
+	errPing := db.Ping()
+	if errPing != nil {
+		log.Fatal("error connect to db ", errPing.Error())
+		// panic("error connect db")
 	} else {
 		fmt.Println("success connect to DB")
 	}
